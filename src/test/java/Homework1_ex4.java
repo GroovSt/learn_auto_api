69c91ff7f362f86179a6d53c726ea2a59eed8a1d
GroovSt <groovst@gmail.com>
2022-12-07 06:21:50 +0500

    Ex10 and first homework

---
+++ src/test/java/Homework1_ex4.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Homework1_ex4 {
+
+    @Test
+    public void homeworkEx4() {
+        Response answer = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        answer.prettyPrint();
+    }
+
+}
