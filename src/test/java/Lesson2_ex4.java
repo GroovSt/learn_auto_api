6bee888956f7b7b75ec1fb01fa88d3f1e5ededea
GroovSt <groovst@gmail.com>
2022-12-11 22:42:29 +0500

    .

--- src/test/java/Lesson2_ex4.java
+++ src/test/java/Lesson2_ex4.java
@@ -2,10 +2,14 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class Lesson1_ex10 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lesson2_ex4 {
 
     @Test
-    public void firstTest() {
+    public void lessonTwoEx4() {
+
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
